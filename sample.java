d6e158c2c85d8903f2ac9e47e737898f059fab62
Manichandana <manichandana230@gmail.com>
2024-08-29 12:01:34 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -1,6 +1,6 @@
 public class sample{
   public static void main(String args[]){
-    String s="system";
-    System.out.println("The password is:" +s);
+    String s="Hello World";
+    System.out.println("This is the first java program:" +s);
   }
 }
